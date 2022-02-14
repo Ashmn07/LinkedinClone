5a011c09f3b5f9dca0e625528cd28b813491163a
Ashmn07 <ashwin.menon202@gmail.com>
2022-02-14 18:58:04 +0530

    Sign In Functionality Added using Next-Auth

--- pages/home.jsx
+++ pages/home.jsx
@@ -6,8 +6,9 @@ import OndemandVideoSharpIcon from "@mui/icons-material/OndemandVideoSharp";
 import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
 import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
 import Head from 'next/head';
+import { getProviders, signIn } from "next-auth/react";
 
-export default function Home() {
+export default function Home({providers}) {
   return (
     <div className="space-y-10 relative">
       <Head>
@@ -25,17 +26,24 @@ export default function Home() {
               <HeaderLink Icon={OndemandVideoSharpIcon} text="Learning" />
               <HeaderLink Icon={BusinessCenterIcon} text="Jobs" />
             </div>
-            <div className="pl-4">
-              <button className="text-blue-700 font-semibold rounded-full border 
-              border-blue-700 px-5 py-1.5 transition-all hover:border-2">
-                Sign In
-              </button>
+            {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <div className="pl-4">
+                <button
+                  className="text-blue-700 font-semibold rounded-full border border-blue-700 px-5 
+                  py-1.5 transition-all hover:border-2"
+                  onClick={() => signIn(provider.id, { callbackUrl: "/" })}>
+                  Sign in
+                </button>
+              </div>
             </div>
+          ))}
           </div>
       </header>
       <main className="flex flex-col xl:flex-row items-center max-w-screen-lg mx-auto">
         <div className="space-y-6 xl:space-y-10">
-          <h1 className="text-3xl md:text-5xl text-amber-800/80 max-w-xl !leading-snug pl-4 xl:pl-0">
+          <h1 className="text-3xl md:text-5xl text-amber-800/80 max-w-xl 
+          !leading-snug pl-4 xl:pl-0">
             Welcome to your professional community
           </h1>
           <div className="space-y-4">
@@ -59,4 +67,14 @@ export default function Home() {
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
 }
